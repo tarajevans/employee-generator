e4ca3d1a8f85db0ab9b8feee5194be83ee350e60
Tara Evans <tarajevans@hotmail.com>
2022-08-21 17:14:37 -0400

    Intern tested and written

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,12 @@
+const Intern = require('../lib/Intern.js');
+
+test('create a Intern object', ()=>{
+    const intern = new Intern("Becky", 236, "Becky@becky.com", "some School");
+
+    expect(intern.name).toBe("Becky");
+    expect(intern.id).toBe(236);
+    expect(intern.email).toBe("Becky@becky.com");
+    expect(intern.getSchool()).toBe("some School");
+    expect(intern.getRole()).toBe("Intern");
+})
+
